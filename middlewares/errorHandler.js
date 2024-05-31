f1832f6789673db0beaaa0921a887f1966551513
Allen <valternatean13@gmail.com>
2024-05-31 09:48:37 +0700

    feat: logger for debugging purpose

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,9 +4,10 @@ import ERRORS from "../configs/error.js"
 
 dotenv.config();
 export default function errorHandler(err, req, res, next) {
+    console.error(err)
     const {status_code, msg} = ERRORS[err.code] || {status_code: 500, msg: "Unknown server error"};
     return res.status(status_code).send({
         message: msg,
-        log: (process.env.NODE_ENV == "DEVEL"? err.detail: "[DISCLOSED]")
+        log: (process.env.NODE_ENV == "DEVEL"? err.detail: undefined)
     })
 }
