0abdec6e96e7aa1c0eb4b63687e10d3f3304c942
Allen <valternatean13@gmail.com>
2024-05-31 18:09:28 +0700

    ref: make JWT secret chosen dynamically

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -2,6 +2,12 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
+const secret = {
+    "DEVEL": process.env.DEVEL_JWT_SECRET,
+    "STAGING": process.env.STAGING_JWT_SECRET,
+    "PRODUCTION": process.env.PRODUCTION_JWT_SECRET,
+}
+
 export default function authJWT(req, res, next) {
     const token = req.headers.authorization?.replace("Bearer ", "");
 
@@ -9,7 +15,7 @@ export default function authJWT(req, res, next) {
         return res.status(401).json({ code: "token_missing_or_invalid", message: "No token provided" });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedData) => {
+    jwt.verify(token, secret[process.env.NODE_ENV], (err, decodedData) => {
         if (err) {
             return res.status(403).json({ code: "token_invalid", message: "Invalid token" });
         }
