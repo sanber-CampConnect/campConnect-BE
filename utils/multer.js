63e89130cdf4b74e37de3c3a43ce998677d1701c
Allen <valternatean13@gmail.com>
2024-06-10 07:15:38 +0700

    debug: image not saved in deployment

--- utils/multer.js
+++ utils/multer.js
@@ -1,4 +1,7 @@
 export const imageOnly = function(req, file, callback) {
     if(file.mimetype.startsWith("image/")) callback(null, true)
-    else callback(new Error("Only images are allowed"), false)
+    else {
+        console.log("Non-image file uploaded")
+        callback(new Error("Only images are allowed"), false)
+    }
 }
