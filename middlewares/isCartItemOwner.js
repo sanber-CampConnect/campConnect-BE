6a6b362784229d45802fe2fdce04e1cf2ace9462
Allen <valternatean13@gmail.com>
2024-06-12 06:58:15 +0700

    fix: wrong checking logic

--- middlewares/isCartItemOwner.js
+++ middlewares/isCartItemOwner.js
@@ -1,13 +1,29 @@
 import CartItems from "../models/CartItems.js"
+import DBConnection from "../models/DBConnection.js";
 
-export default function isCartOwner(req, res, next) {
+export default function isCartItemOwner(req, res, next) {
     if(req.user.role == "admin") next()
 
-    const cartItemId = req.params.id;
-    CartItems.getById(req.user.id)
+    // TODO: make a stored procedure to do all of these things with one DB call
+    const cartItemId = Number(req.params.id);
+    CartItems.getById(cartItemId) 
         .then(result => {
-            if(result.length == 0) throw {code: "not_found", msg: `No CartItem with id ${cartItemId} has found`}
-            if(result[0].id != cartItemId) throw {code: "not_owner", msg: `CartItem with id ${cartItemId} belongs to other user`}
+            if(result.length == 0) throw {
+                code: "not_found", 
+                msg: `No CartItem with id ${cartItemId} has found`
+            }
+
+            return DBConnection.query(
+                "SELECT 1 from Carts WHERE id = ? AND user_id = ?", 
+                [result[0].cart_id, req.user.id]
+            )
+        })
+        .then(result => {
+            if(result.length == 0) throw {
+                code: "not_owner", 
+                msg: `CartItem with id ${cartItemId} owned by other user`
+            }
+
             next();
         })
         .catch(err => next(err))
