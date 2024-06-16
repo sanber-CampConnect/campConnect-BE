413f488680da5e1f795790846e615e2fd48c04d5
Allen <valternatean13@gmail.com>
2024-06-16 09:33:38 +0700

    fix: wrong table name and response property type
    fix: wrong SP syntax

--- scripts/database/stored_procedures.sql
+++ scripts/database/stored_procedures.sql
@@ -9,11 +9,10 @@ DROP PROCEDURE IF EXISTS `Product_delete`;
 DELIMITER //
 
 CREATE PROCEDURE `User_add`(
-    IN ifullname VARCHAR(64) NOT NULL,
-    IN iusername VARCHAR(32) NOT NULL,
-    IN iemail VARCHAR(64) UNIQUE NOT NULL,
-    IN ipassword VARCHAR(64) NOT NULL,
-    IN iphone VARCHAR(16) DEFAULT NULL,
+    IN ifullname VARCHAR(64),
+    IN iusername VARCHAR(32),
+    IN iemail VARCHAR(64),
+    IN ipassword VARCHAR(64),
     OUT onew_user_id INTEGER
 )
 BEGIN
@@ -22,6 +21,7 @@ BEGIN
     SET onew_user_id = LAST_INSERT_ID();
     INSERT INTO Carts(`user_id`) VALUES( onew_user_id );
 END;
+//
 
 CREATE PROCEDURE `User_delete`(
     IN iuser_id INTEGER
@@ -33,26 +33,28 @@ BEGIN
     DELETE FROM Carts WHERE user_id = iuser_id;
     DELETE FROM Users WHERE id = iuser_id;
 END;
-
+//
 
 CREATE PROCEDURE `Order_add`(
     IN iuser_id INTEGER,
-    IN iinvoice_number INTEGER UNSIGNED NOT NULL,
-    IN imethod ENUM("tunai", "transfer") NOT NULL,
-    IN itotal_items INTEGER UNSIGNED NOT NULL,
-    IN itotal_price INTEGER UNSIGNED NOT NULL,
+    IN iinvoice_number VARCHAR(32),
+    IN imethod VARCHAR(8),
+    IN itotal_items INTEGER UNSIGNED,
+    IN itotal_price INTEGER UNSIGNED,
     OUT onew_order_id INTEGER
 )
 BEGIN
     INSERT INTO Orders(`user_id`, `payment_due`)
         VALUES(iuser_id, DATE_ADD(NOW(), INTERVAL 3 DAY));
     SET onew_order_id = LAST_INSERT_ID();
-    INSERT INTO Transactions(`order_id`, `invoice_number`, `method`, `total_price`, `total_items`) 
-        VALUES( onew_order_id, iinvoice_number, imethod, itotal_price, itotal_items);
+    INSERT INTO Transactions(`order_id` `invoice_number`, `method`, `total_price`, `total_items`) 
+        VALUES(onew_order_id, iinvoice_number, imethod, itotal_price, itotal_items);
+    SELECT onew_order_id AS insertId;
 END;
+//
 
 CREATE PROCEDURE `Order_delete`(
-    IN iorder_id INTEGER,
+    IN iorder_id INTEGER
 )
 BEGIN
     DELETE FROM `Transactions` WHERE `order_id` = iorder_id;
@@ -60,7 +62,7 @@ BEGIN
     DELETE FROM `Reviews` WHERE `order_id` = iorder_id;
     DELETE FROM `Orders` WHERE `id` = iorder_id;
 END;
-
+//
 
 
 CREATE PROCEDURE `Product_delete`(
@@ -71,4 +73,5 @@ BEGIN
     DELETE FROM `Products` WHERE `id` = iproduct_id;
 END;
 //
+
 DELIMITER ;
