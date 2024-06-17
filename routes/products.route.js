1faef4280a8c41efd94ec3f184607e881a85194d
Allen <valternatean13@gmail.com>
2024-06-18 05:40:44 +0700

    fix: `Products` endpoint restricted only to authenticated users

--- routes/products.route.js
+++ routes/products.route.js
@@ -32,8 +32,8 @@ const upload = multer({
 })
 
 const ROUTER = express.Router();
-ROUTER.get("/", authenticated_only_middlewares, controller.index);
-ROUTER.get("/:id", authenticated_only_middlewares, controller.findOne);
+ROUTER.get("/", controller.index);
+ROUTER.get("/:id", controller.findOne);
 ROUTER.post("/", [...admin_only_middlewares, upload.single("image")], controller.store);
 ROUTER.put("/:id", [...admin_only_middlewares, upload.single("image")], controller.edit);
 ROUTER.delete("/:id", admin_only_middlewares, controller.destroy);
