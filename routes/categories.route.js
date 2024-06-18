14318f3db02f1acf6894764768b8c0bd060c6ab6
vimoncibel <alyavicom082@gmail.com>
2024-06-18 16:09:59 +0700

    fix: delete auth

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -4,8 +4,8 @@ import { authenticated_only_middlewares, admin_only_middlewares } from "../confi
 
 // TODO: Protect using RBAC
 const ROUTER = express.Router();
-ROUTER.get("/", authenticated_only_middlewares, controller.index);
-ROUTER.get("/:id", authenticated_only_middlewares, controller.findOne);
+ROUTER.get("/", controller.index);
+ROUTER.get("/:id", controller.findOne);
 ROUTER.post("/", admin_only_middlewares, controller.store);
 ROUTER.put("/:id", admin_only_middlewares, controller.edit);
 ROUTER.delete("/:id",admin_only_middlewares, controller.destroy);
