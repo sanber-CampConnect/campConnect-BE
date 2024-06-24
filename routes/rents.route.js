041ac521f158182c3327b89922b8d18b4f538355
Allen <valternatean13@gmail.com>
2024-06-24 18:44:53 +0700

    feat: add endpoint to finish a rent

--- routes/rents.route.js
+++ routes/rents.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import controller from "../controllers/rents.controller.js";
-import { authenticated_only_middlewares } from "../configs/accessMiddlewares.js";
+import { admin_only_middlewares, authenticated_only_middlewares } from "../configs/accessMiddlewares.js";
 
 const ROUTER = express.Router();
 
@@ -10,5 +10,6 @@ ROUTER.get("/orderItem/:orderItemId", authenticated_only_middlewares, controller
 ROUTER.post("/", authenticated_only_middlewares, controller.store);
 ROUTER.put("/:id", authenticated_only_middlewares, controller.edit);
 ROUTER.delete("/:id", authenticated_only_middlewares, controller.destroy);
+ROUTER.patch("/:id", admin_only_middlewares, controller.finishRent)
 
 export default ROUTER;
